0971179fe56d86276e0780ee7424298a5713fbb7
Chris Gitmed <cgitmed@gmail.com>
2021-01-31 03:07:13 +0000

    Created a row under the PreviewBox with four input elements.

--- src/components/border-radius-preview.jsx
+++ src/components/border-radius-preview.jsx
@@ -4,7 +4,16 @@ import TestSquare from './test-square';
 export default class BorderRadiusPreview extends React.Component {
   render() {
     return (
-      <TestSquare/>
+      <>
+        <TestSquare/>
+        <div className="input-row">
+          <span>Border: </span>
+          <span><input type="text" /> px</span>
+          <span><input type="text" /> px</span>
+          <span><input type="text" /> px</span>
+          <span><input type="text" /> px</span>
+        </div>
+      </>
     )
   }
 }
